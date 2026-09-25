de1706d6d4d2ce2a6f1e5048c5ffb36a6d6463cc
surajalagouda16 <164084216+surajalagouda16@users.noreply.github.com>
2026-09-25 20:37:06 +0530

    Create Area_rectangle.cpp

---
+++ Programs/01_CPP_Fundamentals/Area_rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int len,bre,area;
+    cout<<"Enter length and breadth:"<<endl;
+    cin>>len;
+    cin>>bre;
+    area=len*bre;
+    cout<<"Area:"<<area<<endl;
+    return 0;
+}
